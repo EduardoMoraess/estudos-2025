4d4d3a2b85538287b42e260e4ad7de90b682ff67
EduardoMoraes <eduzinho2118@gmail.com>
2025-01-17 20:47:16 -0300

    assuntos novos de javascript

---
+++ aulas_JS/aula01.js
@@ -0,0 +1,12 @@
+/*function saudacao(nome){
+    return `ola, ${nome}!`
+}
+console.log(saudacao,('eduardo'))*/
+
+const pessoa = {
+    nome: 'edu',
+    idade: 21,
+    habilidade: ['JS','PY','CSS3','HTML5']
+}
+console.log(pessoa.nome); // Acessar propriedade
+console.log(pessoa.habilidade); // Acessar array
