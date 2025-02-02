d48e3f9992efd70d36911687654bf4c3f364097e
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-02 00:51:14 -0300

    pra

---
+++ aulas_JS/pratica.js
@@ -0,0 +1,7 @@
+/*
+Crie um script em JS que: 
+1-peça ao usuario dois numeros usando prompt()
+2-Converta para numero
+3-Some os dois numeros
+4-Exiba o resultado no console(console.log()) ou em um alert(alert())
+*/
