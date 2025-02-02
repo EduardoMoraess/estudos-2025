5aa4f1759f649e25eb05ae7d1f92a075d4053c9f
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-02 00:54:18 -0300

    asda

--- aulas_JS/pratica.js
+++ aulas_JS/pratica.js
@@ -5,3 +5,10 @@ Crie um script em JS que:
 3-Some os dois numeros
 4-Exiba o resultado no console(console.log()) ou em um alert(alert())
 */
+
+let numero1 = Number(prompt('Digite um numero:'))
+let numero2 = Number(prompt('Digite outro numero:'))
+
+let soma = numero1 + numero2
+
+console.log(`A soma entre ${numero1} e ${numero2} vale a ${soma}`)
