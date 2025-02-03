1ff4473b72931b8adfa0cbec3bc6592fdf59f925
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-02 21:43:28 -0300

    asda

--- aulas_JS/pratica/pratica3.js
+++ aulas_JS/pratica/pratica3.js
@@ -1,8 +1,16 @@
-let numero = 124
+/*
+Exercício: Verificador de Número Par ou Ímpar Crie um programa que:
+1-Peça para o usuário inserir um número.
+2-Verifique se o número é par ou ímpar.
+3-Exiba o resultado usando alert().
+*/
 
-if(numero % 2 === 0){
-    console.log('numero par')
+
+let numero = 124;
+
+if (numero % 2 === 0) {
+    console.log('numero par');
 }
-else{
-    console.log('numero impar')
+else {
+    console.log('numero impar');
 }
