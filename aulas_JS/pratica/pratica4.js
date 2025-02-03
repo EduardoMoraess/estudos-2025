a7ef37d00738277706b61f7744c80d352cd1bd19
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-03 20:49:11 -0300

    awsda

---
+++ aulas_JS/pratica/pratica4.js
@@ -0,0 +1,18 @@
+/*
+Exercício: Calculadora de Tabuada
+Crie um programa que:
+1-Peça para o usuário inserir um número.
+2-Exiba a tabuada desse número (de 1 a 10) no console.
+*/
+
+let numero = Number(prompt('digite um numero'))
+
+if(!NaN(numero)){
+    console.log(`Tabuada do numero ${numero}`)
+
+    for (let i =1; i <=10; i++){
+        console.log(`${numero} x ${i} = ${numero * i}`)
+    }
+}else{
+    console.log('ERRO! numero não encontrado')
+}
