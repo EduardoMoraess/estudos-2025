c0849751ee67de1f9af8e5e034c7761eb5a9efa3
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-03 20:55:22 -0300

    prompt

--- aulas_JS/pratica/pratica4.js
+++ aulas_JS/pratica/pratica4.js
@@ -5,13 +5,14 @@ Crie um programa que:
 2-Exiba a tabuada desse número (de 1 a 10) no console.
 */
 
-let numero = 2
+let numero = Number(prompt('Digite um numero'))
 
 if(!isNaN(numero)){
     console.log(`Tabuada do numero ${numero}`)
 
     for (let i =1; i <=10; i++){
         console.log(`${numero} x ${i} = ${numero * i}`)
+        
     }
 }else{
     console.log('ERRO! numero não encontrado')
