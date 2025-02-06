5aae62b31d49f42c45bae1e9830b7c2db746f46e
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-05 22:01:04 -0300

    testando

--- aulas_JS/pratica/pratica9.js
+++ aulas_JS/pratica/pratica9.js
@@ -5,3 +5,12 @@ Crie um programa que:
 2-Verifique se o nome de usuário contém pelo menos 6 caracteres.
 3-Exiba uma mensagem de sucesso ou erro, dependendo da validação.
 */
+
+let nome_user = prompt('Dgite seu nome de usuario!:');
+
+if(nome_user.length >= 6){
+    alert('Seu nome de usuario é valido!');
+}
+else{
+    alert('Seu nome de usuario não é valido!');
+}
