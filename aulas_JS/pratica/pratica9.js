1ff685dff69fbdbc9b87bf7ee3b8f8ef4241e555
EduardoMoraes <eduzinho2118@gmail.com>
2025-02-05 21:48:14 -0300

    asda

---
+++ aulas_JS/pratica/pratica9.js
@@ -0,0 +1,7 @@
+/*
+Exercício: Verificador de Nome de Usuário
+Crie um programa que:
+1-Peça para o usuário digitar o seu nome de usuário.
+2-Verifique se o nome de usuário contém pelo menos 6 caracteres.
+3-Exiba uma mensagem de sucesso ou erro, dependendo da validação.
+*/
